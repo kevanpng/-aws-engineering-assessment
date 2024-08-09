5e0145ff478af410dcdba45a4f5cb8ef3182f647
kevan <kevanpngyeowwei@gmail.com>
2024-08-09 20:40:35 +0800

    remove comments

--- terraform/lambda/index.py
+++ terraform/lambda/index.py
@@ -5,22 +5,6 @@ import json
 def lambda_handler(event, context):
     print("lambda triggered")
     print(event)
-    # {'Records': [
-    # {'eventVersion': '2.1',
-    # 'eventSource': 'aws:s3',
-    # 'awsRegion': 'eu-central-1',
-    # 'eventTime': '2024-08-09T06:26:12.352Z',
-    # 'eventName': 'ObjectCreated:Put',
-    # 'userIdentity': {'principalId': 'AIDAJDPLRKLG7UEXAMPLE'},
-    # 'requestParameters': {'sourceIPAddress': '127.0.0.1'},
-    # 'responseElements': {'x-amz-request-id': '71914e40', 'x-amz-id-2': 'eftixk72aD6Ap51TnqcoF8eFidJG9Z/2'},
-    # 's3': {
-    #   's3SchemaVersion': '1.0', 'configurationId': 'lambda1',
-    #   'bucket': {
-    #       'name': 'swisscom-bucket-1', 'ownerIdentity': {'principalId': 'A3NL1KOZZKExample'},
-    #       'arn': 'arn:aws:s3:::swisscom-bucket-1'
-    #    },
-    #   'object': {'key': 'README.md', 'sequencer': '0055AED6DCD90281E5', 'versionId': 'Wv_3zzpSQ6X62lq_8DpYcg', 'size': 1483, 'eTag': 'aea5bc4ed32903981ef6aa451bc12086'}}}]}
     object_key = get_object_key(event)
     print(object_key)
     client = boto3.client('stepfunctions')
@@ -30,7 +14,6 @@ def lambda_handler(event, context):
     response = client.start_execution(
         stateMachineArn='arn:aws:states:eu-central-1:000000000000:stateMachine:swisscom-sfn',
         input=input_json,
-        # traceHeader='string'
     )
     print(response)
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,6 @@ locals {
   region      = "eu-central-1"
 }
 
-#resource "random_pet" "this" {
-#  length = 2
-#}
-
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
@@ -24,25 +20,6 @@ module "s3_bucket" {
   }
 }
 
-#############################################
-# Using packaged function from Lambda module
-#############################################
-
-locals {
-  package_url = "https://raw.githubusercontent.com/terraform-aws-modules/terraform-aws-lambda/master/examples/fixtures/python3.8-zip/existing_package.zip"
-  downloaded  = "downloaded_package_${md5(local.package_url)}.zip"
-}
-
-#resource "null_resource" "download_package" {
-#  triggers = {
-#    downloaded = local.downloaded
-#  }
-#
-#  provisioner "local-exec" {
-#    command = "curl -L -o ${local.downloaded} ${local.package_url}"
-#  }
-#}
-
 module "lambda_function1" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "~> 3.0"
@@ -52,9 +29,6 @@ module "lambda_function1" {
   runtime       = "python3.8"
 
   source_path = "./lambda"
-
-#  create_package         = false
-#  local_existing_package = local.downloaded
 }
 
 module "step_function" {
@@ -108,7 +82,6 @@ module "dynamodb_table" {
 
   name                        = "${local.company}-ddb-1"
   hash_key                    = "FileName"
-#  range_key                   = "title"
   table_class                 = "STANDARD"
   deletion_protection_enabled = false
 
@@ -120,78 +93,17 @@ module "dynamodb_table" {
   ]
 }
 
-
-#
-#module "lambda_function2" {
-#  source  = "terraform-aws-modules/lambda/aws"
-#  version = "~> 3.0"
-#
-#  function_name = "${local.company}-lambda2"
-#  handler       = "index.lambda_handler"
-#  runtime       = "python3.8"
-#
-#  create_package         = false
-#  local_existing_package = local.downloaded
-#}
-#
-#module "sns_topic1" {
-#  source  = "terraform-aws-modules/sns/aws"
-#  version = "~> 3.0"
-#
-#  name_prefix = "${local.company}-2"
-#}
-#
-#module "sns_topic2" {
-#  source  = "terraform-aws-modules/sns/aws"
-#  version = "~> 3.0"
-#
-#  name_prefix = "${local.company}-2"
-#}
-#
-#resource "aws_sqs_queue" "this" {
-#  count = 2
-#  name  = "${local.company}-${count.index}"
-#}
-#
-## SQS policy created outside of the module
-#data "aws_iam_policy_document" "sqs_external" {
-#  statement {
-#    effect  = "Allow"
-#    actions = ["sqs:SendMessage"]
-#
-#    principals {
-#      type        = "Service"
-#      identifiers = ["s3.amazonaws.com"]
-#    }
-#
-#    resources = [aws_sqs_queue.this[0].arn]
-#  }
-#}
-#
-#resource "aws_sqs_queue_policy" "allow_external" {
-#  queue_url = aws_sqs_queue.this[0].id
-#  policy    = data.aws_iam_policy_document.sqs_external.json
-#}
-#
 module "all_notifications" {
-#  source = "../terraform-aws-modules/s3-bucket/aws/modules/notification"
   source = "./.terraform/modules/s3_bucket/modules/notification"
 
   bucket = module.s3_bucket.s3_bucket_id
 
-#  eventbridge = true
-
-  # Common error - Error putting S3 notification configuration: InvalidArgument: Configuration is ambiguously defined. Cannot have overlapping suffixes in two rules if the prefixes are overlapping for the same event type.
 
   lambda_notifications = {
     lambda1 = {
       function_arn  = module.lambda_function1.lambda_function_arn
       function_name = module.lambda_function1.lambda_function_name
       events        = ["s3:ObjectCreated:Put"]
-#      filter_prefix = "prefix/"
-#      filter_suffix = ".json"
     }
   }
-
-#  policy = false
 }
